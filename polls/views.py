9e2e7e4ec1ceaa5cb98ad37ef6c5f48d33609fc8
stephanie <luz.stephanie@gmail.com>
2018-05-10 23:13:32 -0400

    Serializers

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from rest_framework import status
+from rest_framework import status
 from .models import Stock
 from .serializers import stockSerializer
 
